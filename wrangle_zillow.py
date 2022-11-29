dfef48d5cffe19e1cf4831e11092035c52a78b23
Yvette Ibarra <yvette.ibarra01@gmail.com>
2022-11-28 18:16:47 -0600

    add wrangle.py file for zillow

---
+++ wrangle_zillow.py
@@ -0,0 +1,121 @@
+import env
+import os
+
+import numpy as np
+import pandas as pd
+
+###################################         AQUIRE   #########################
+def fresh_zillow_data():
+    ''' fresh_zillow_data acquires Zillow data using properties_2017 
+    table from Code up Data Base. '''
+    
+    sql_query = '''
+            SELECT
+            prop.*,
+            predictions_2017.logerror as log_error,
+            predictions_2017.transactiondate as transaction_date,
+            air.airconditioningdesc as aircondition,
+            arch.architecturalstyledesc as architectural_style,
+            build.buildingclassdesc as bulding_class,
+            heat.heatingorsystemdesc as heat_systems,
+            landuse.propertylandusedesc as land_use,
+            story.storydesc as story,
+            construct.typeconstructiondesc as construction_type
+            FROM properties_2017 prop
+            JOIN (
+                SELECT parcelid, MAX(transactiondate) AS max_transactiondate
+                FROM predictions_2017
+                GROUP BY parcelid
+            ) as pred USING(parcelid)
+            JOIN predictions_2017 ON pred.parcelid = predictions_2017.parcelid
+                                  AND pred.max_transactiondate = predictions_2017.transactiondate
+            LEFT JOIN airconditioningtype air USING (airconditioningtypeid)
+            LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid)
+            LEFT JOIN buildingclasstype build USING (buildingclasstypeid)
+            LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid)
+            LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid)
+            LEFT JOIN storytype story USING (storytypeid)
+            LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid)
+            WHERE prop.latitude IS NOT NULL
+              AND prop.longitude IS NOT NULL
+              AND transactiondate <= '2017-12-31'
+              AND propertylandusedesc like '%%Single%%'
+            '''
+
+    
+    df = pd.read_sql(sql_query, env.get_db_url('zillow'))
+
+    return df
+
+def get_zillow_data(new = False):
+        ''' get_zillow_data checks to see if a csv exist localy
+        and retrieves if not calls on fresh_zillow_data to acquire Zillow 
+        data using properties_2017 table from Code up Data Base and saves csv locally 
+        '''
+   
+
+        filename = 'zillow.csv'
+
+        # obtain cvs file
+        if (os.path.isfile(filename) == False) or (new == True):
+            df = fresh_zillow_data()
+            #save as csv
+            df.to_csv(filename,index=False)
+
+        #cached data
+        else:
+            df = pd.read_csv(filename)
+
+        return df
+
+####################################### NULL VALUES ############################################
+def null_counter(df):
+    ''' null_counter takes in a dataframe anc calculates the percent and amount of null cells in each column and row
+    returns a dataframe with the results'''
+    # name of dataframe names
+    new_columns = ['name', 'num_rows_missing', 'pct_rows_missing']
+    
+    # create data frame
+    new_df = pd.DataFrame(columns = new_columns)
+   
+    # for loop to calculate missing /percent by columns
+    for col in list(df.columns):
+        num_missing = df[col].isna().sum()
+        pct_missing = num_missing / df.shape[0]
+        
+        # create data frame
+        add_df = pd.DataFrame([{'name': col,
+                               'num_rows_missing': num_missing,
+                               'pct_rows_missing': pct_missing}])
+       
+        # concat and index by row by seting axis to 0   
+        new_df = pd.concat([new_df, add_df], axis = 0)
+        
+    # sets the index name
+    new_df.set_index('name', inplace = True)
+    
+    return new_df
+
+def null_dropper(df,prop_required_column,prop_requred_row):
+
+    ''' null_dropper takes in a dataframe a percent of required columns and rows to keep columns.
+    all columns and rows outside of the null threshold will be dropped
+    returns a clean dataframe dropped nulls'''
+    
+    # this is a decimal = 1- decimal
+    prop_null_column = 1-prop_required_column
+    
+    # for columns, check null percentage and drop if a certain proportion is null (set by definition)
+    for col in list(df.columns):
+        null_sum = df[col].isna().sum()
+        null_pct = null_sum / df.shape[0]
+        
+        if null_pct > prop_null_column:
+            df.drop(columns = col, inplace = True)
+    
+    # for rows, drop if a certain proportion is null. (set by definition)
+    row_threshold = int(prop_requred_row * df.shape[1])
+    
+    df.dropna(axis = 0, thresh=row_threshold, inplace = True)
+    
+    return df
